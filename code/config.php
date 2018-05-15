26235a5889658f20ee2dd3c634da6e12e9e1a85d
Matthew Smart <msmart1994@gmail.com>
2018-05-15 06:56:18 -0400

    Delete config.php

--- code/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Used to connecting to the database and carrying session variables
-session_start();
-$host = "localhost"; /* Host name */
-$user = "root"; /* User */
-$password = ""; /* Password */
-//mysql connection to server
-$conn = mysql_connect($host, $user, $pass);
-?>
